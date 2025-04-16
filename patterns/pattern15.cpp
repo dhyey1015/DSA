efdbbfafd8021265d48fa8e99e16b904d205f36b
dhyey1015 <dhyeykakadiya1015@gmail.com>
2025-04-17 00:25:50 +0530

    done with pattern 15

---
+++ patterns/pattern15.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n = 5;
+    char ch = 'A'; 
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j <= i; j++){
+            cout << ch;
+        }
+        cout << "\n";
+        ch += 1;
+    }
+}
