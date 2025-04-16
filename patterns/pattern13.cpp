d51f3973977f8b0075f7adbad7661b0054d564f6
dhyey1015 <dhyeykakadiya1015@gmail.com>
2025-04-17 00:08:42 +0530

    done with patter 13

---
+++ patterns/pattern13.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n = 5;
+    int count = 1;
+    for(int i = 0; i <= n; i++){
+        for(int j = 0; j < i; j++){
+            cout << " " << count;
+            count++;
+        }
+        cout << "\n";
+    }
+}
