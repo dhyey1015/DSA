15a62b72c93ec7f3ed17a878fa6c7e7bbea8a4d1
dhyey1015 <dhyeykakadiya1015@gmail.com>
2025-04-18 03:32:39 +0530

    done with unordered_set

---
+++ STL-in-cpp/unordered_set.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    unordered_set<int> us;
+
+    //lower_bound and upper_bound functions does not work
+    //rest all functions are same as of set
+    //it does not store values in any particular order
+    //it only stores unique elements
+    //it has better time complexity then set in most case 
+    //time complexity is O(1) some time 1 in 1 million times it becomes O(n)
+    return 0;
+}
