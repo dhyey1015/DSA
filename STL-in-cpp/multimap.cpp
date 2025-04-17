b01d0f777aa0716596e8a48cb46f746d1ff92490
dhyey1015 <dhyeykakadiya1015@gmail.com>
2025-04-18 03:50:29 +0530

    done with unordered_map and multimap

--- STL-in-cpp/multimap.cpp
+++ STL-in-cpp/multimap.cpp
@@ -7,6 +7,5 @@ int main(){
     //everything is same as map only it can store multiple keys 
     //we can store dublicate keys over here 
     // we cand do the {{1,2}, {1,3}}
-
     return 0;
 }
---
+++ STL-in-cpp/unordered_map.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    unordered_map<int, int>mpp;
+    // it is similar to map only things are:
+    // the map will not be sorted and map will have unique keys 
+    return 0;
+}
