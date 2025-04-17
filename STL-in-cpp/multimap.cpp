2d15fcf1123c2119ce9462ae353c02d9e887e211
dhyey1015 <dhyeykakadiya1015@gmail.com>
2025-04-18 03:44:40 +0530

    done with multimap

---
+++ STL-in-cpp/multimap.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    multimap<int, int> mpp;
+
+    //everything is same as map only it can store multiple keys 
+    //we can store dublicate keys over here 
+    // we cand do the {{1,2}, {1,3}}
+
+    return 0;
+}
