2c224190a3c7774f3e3d1e60f7a68538e31e271b
dhyey1015 <dhyeykakadiya1015@gmail.com>
2025-04-20 23:33:03 +0530

    added some good comments to the code

--- basic-math/give-all-divisors-of-number.cpp
+++ basic-math/give-all-divisors-of-number.cpp
@@ -5,9 +5,12 @@ int main(){
 
     int n;
     cin >> n;
-    set<int> s; // store the values in set if you want it in sorted order;
+    set<int> s; // store the values in set if you want it in sorted order
     cout << "All divisors of number: "<< n << endl;
-    for(int i = 1; i <= sqrt(n); i++){
+    // i = 6 
+    //  6 * 6 <= 36 -> true
+    // 7 * 7 <= 36 -> false
+    for(int i = 1; i * i <= n; i++){
         if(n % i == 0){
             int multipliedValue = n / i; // getting the value that will be multipled with i
             if(i != multipliedValue){
